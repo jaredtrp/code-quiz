78ecbd5764f1169f8ac299f005c761b003a57c6b
jaredtrp <jaredtrp@gmail.com>
2021-12-04 18:39:26 -0600

    updated CSS styling and wrote pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,23 @@
 // THEN I can save my initials and score
 
 
+
+// Page begins with instructions and a start button to begin
+// On start button click the following happens:
+// Hide Instructions and start button
+// Start timer
+// Reveal first question and answers
+
+// When an answer is selected, verify if correct
+// If correct display "Correct!", change corrent button bg to green, Add 1 to score
+// If incorrect display "Wrong! Lose 2 Seconds", color button bg red, color correct button bg green, and deduct 2 seconds from timer
+// Automatically switch to next question after 1.5 seconds
+
+// For next question, the following happens:
+// clear previous question and answers
+// display next question and answers
+
+// When time equals zero, the following happens:
+// Remove question and answer
+// Display "Game Over" and score
+// give link to high scores
